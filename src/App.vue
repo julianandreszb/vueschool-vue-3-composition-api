c2957e1cdc8793826844b0e66d2937eb4186d68b
jzapata <julianandreszb@gmail.com>
2022-12-10 02:05:13 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import YummyMeal from "./components/YummyMeal.vue";
+
+const addToCart = name => console.log('addToCart.name', name);
+
+</script>
+
+<template>
+  <YummyMeal
+      name="Bandeja Paisa"
+      :price="Math.trunc(30)"
+      @addToCart="addToCart"
+  ></YummyMeal>
+  <HelloWorld msg="Vite + Vue"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/YummyMeal.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+const emit = defineEmits(['addToCart'])
+
+const addToCart = () => emit('addToCart', props.name);
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.name }}</h1>
+    <p>${{ props.price }}</p>
+    <button @click="addToCart" >Add to cart</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
